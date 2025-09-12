ad4737df17f445050842690731ab533555ee9414
Leaf_yj <yukarina11@korea.ac.kr>
2025-09-12 18:24:42 +0900

    챗봇

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes, Link } from "react-router-dom";
+
+// 랜딩페이지
 import SplashPage from './pages/RandingPage/RandingPage.SplashPage';
 import SelectMemberPage from './pages/RandingPage/RandingPage.SelectMemberPage';
+import ViewPage from './pages/RandingPage/RandingPage.SelectMemberPage.ViewPage';
 
+// 사진촬영 페이지
+import PictureSelectMemberPage from './pages/PicturePage/PicturePage.SelectMemberPage';
+import MemberDecoPage from './pages/PicturePage/PicturePage.MemberDecoPage';
+import CameraPage from './pages/PicturePage/PicturePage.CameraPage';
+
+
+// 커뮤니티페이지
 import CommunityHome from './pages/CommunityPage/CommunityHome';
 import CheerArtist from './pages/CommunityPage/Community.CheerArtist';
 import FandomTalk from './pages/CommunityPage/Community.FandomTalk';
 import Vote from './pages/CommunityPage/Community.Vote';
 import SelectMember from './pages/CommunityPage/Community.SelectMember';
-import ViewPage from './pages/RandingPage/RandingPage.SelectMemberPage.ViewPage';
+
+// 홈페이지
+import Home from './pages/HomePage/Home.main';
 
 import ChatApp from './pages/ChatbotPage/Chatbot.Chatapp';
 import ChatChoice from './pages/ChatbotPage/Chatbot.ChatChoice';
@@ -25,6 +37,9 @@ export default function App() {
         <Route path="/selectMember" element={<SelectMemberPage />} />
         <Route path="/selectMember/view" element={<ViewPage />} />
 
+        {/* 홈 path */}
+        <Route path='/home' element={<Home/>} />
+        
         {/* 커뮤니티 path */}
         <Route path="/CommunityHome" element={<CommunityHome />} />
         <Route path='/Community/Vote' element={<Vote />} />
@@ -35,8 +50,11 @@ export default function App() {
         {/* 챗봇 path */}
         <Route path="/ChatChoice" element={<ChatChoice/>} />
         <Route path="/ChatApp/:chatName" element={<ChatApp />} />
+        {/* 사진촬영 path */}
+        <Route path="/picture/select" element={<PictureSelectMemberPage />} /> 
+        <Route path="/picture/deco" element={<MemberDecoPage />} />
+        <Route path="/picture/camera" element={<CameraPage />} />
       </Routes>
     </>
   )
 }
-
