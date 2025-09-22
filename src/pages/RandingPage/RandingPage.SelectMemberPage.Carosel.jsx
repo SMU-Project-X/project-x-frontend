5d13dbd8b204cf090ef00f94e1e8d2d010a63f7d
HanaPark1 <81073857+HanaPark1@users.noreply.github.com>
2025-09-22 12:28:19 +0900

    feat: 캐로셀 요소 추가

--- src/pages/RandingPage/RandingPage.SelectMemberPage.Carosel.jsx
+++ src/pages/RandingPage/RandingPage.SelectMemberPage.Carosel.jsx
@@ -7,6 +7,15 @@ const characters = [
     { id: 2, name: "다온", img: "/Character/다온.png" },
     { id: 3, name: "류하", img: "/Character/류하.png" },
     { id: 4, name: "모아", img: "/Character/모아.png" },
+    { id: 5, name: "세라", img: "/Character/세라.png" },
+    { id: 6, name: "세인", img: "/Character/세인.png" },
+    { id: 7, name: "수린", img: "/Character/수린.png" },
+    { id: 8, name: "아린", img: "/Character/아린.png" },
+    { id: 9, name: "유나", img: "/Character/유나.png" },
+    { id: 10, name: "지원", img: "/Character/지원.png" },
+    { id: 11, name: "채윤", img: "/Character/채윤.png" },
+    { id: 12, name: "현", img: "/Character/현.png" },
+
 ]
 
 export default function Carousel() {
